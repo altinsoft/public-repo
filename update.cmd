6ce5f06a6416e02668bc883ae614d6f1d7186ea9
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-04-22 14:33:40 +0300

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -2,4 +2,4 @@ cls
 C:\Windows\System32\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsUpdate.ps1 --output-document C:\Windows\Temp\WindowsUpdate.ps1 -t 5 -w 5
 powershell.exe -ExecutionPolicy Bypass -File "C:\Windows\Temp\WindowsUpdate.ps1"
 reg import "C:\Windows\Setup\ALTINSOFT\reg.reg" /reg:64
-Restart-Computer
+shutdown /r /t 0
