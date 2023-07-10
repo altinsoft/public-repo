9b759c4137a5bb528e8ffb699475530b2891e489
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-07-10 23:29:39 +0300

    Create vds-update.cmd

---
+++ vds-update.cmd
@@ -0,0 +1,1 @@
+C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsVdsTools.exe --output-document C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe -t 5 -w 5
