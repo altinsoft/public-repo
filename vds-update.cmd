f83bc6cb87a19fddfc1933ec6c7e64f30711cf17
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-07-10 23:53:55 +0300

    Update vds-update.cmd

--- vds-update.cmd
+++ vds-update.cmd
@@ -3,6 +3,6 @@ start /wait C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe
 C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/update_chrome.ps1 --output-document C:\Windows\temp\update_chrome.ps1 -t 5 -w 5
 powershell -ExecutionPolicy Bypass -File C:\Windows\temp\update_chrome.ps1
 C:\Windows\System32\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsUpdate.ps1 --output-document C:\Windows\temp\WindowsUpdate.ps1 -t 5 -w 5
-powershell.exe -ExecutionPolicy Bypass -File "C:\Windows\temp\WindowsUpdate.ps1"
+powershell -ExecutionPolicy Bypass -File "C:\Windows\temp\WindowsUpdate.ps1"
 shutdown /s /t 0
 
