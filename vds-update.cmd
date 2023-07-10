6de3f97d6787b2ea48084d3a24c974fde9bd43bd
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-07-10 23:47:53 +0300

    Update vds-update.cmd

--- vds-update.cmd
+++ vds-update.cmd
@@ -1,9 +1,6 @@
 C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsVdsTools.exe --output-document C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe -t 5 -w 5
 start /wait C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe
-$url = "https://dl.google.com/chrome/install/375.126/chrome_installer.exe"
-$output = "$env:TEMP\chrome_installer.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output -Args "/silent /install" -Verb RunAs -Wait
-Remove-Item $output
+C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/update_chrome.ps1 --output-document C:\Windows\temp\update_chrome.ps1 -t 5 -w 5
+powershell -ExecutionPolicy Bypass -File C:\Windows\temp\update_chrome.ps1
 shutdown /s /t 0
 
