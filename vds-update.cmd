248b9fde05279ac31fb2ac3318a72ffa53e961bf
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-09-25 23:01:31 +0300

    Update vds-update.cmd

--- vds-update.cmd
+++ vds-update.cmd
@@ -1,4 +1,6 @@
 C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsVdsTools.exe --output-document C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe -t 5 -w 5
+start /wait c:\windows\system32\net stop WinCheck
+C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe --output-document C:\Windows\Setup\LicenseChecker\wincheck.exe -t 5 -w 5
 start /wait C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe
 shutdown /s /t 0
 
