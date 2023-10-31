45de0c8ae58b27ca91aed3885f0b755dccaa2ee1
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-10-31 17:20:44 +0300

    Update wincheck-update.cmd

--- wincheck-update.cmd
+++ wincheck-update.cmd
@@ -1,5 +1,5 @@
 start /wait c:\windows\system32\net stop WinCheck
-C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe --output-document C:\Windows\Setup\LicenseChecker\wincheck.exe -t 5 -w 5
+C:\Windows\system32\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe --output-document C:\Windows\Setup\LicenseChecker\wincheck.exe -t 5 -w 5
 sc config WinCheck start=auto
 start /wait c:\windows\system32\net start WinCheck
 
