552cdf19bc8e597b638c869fb337474a98b66202
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-11-17 17:25:18 +0300

    Update wincheck-update.cmd

--- wincheck-update.cmd
+++ wincheck-update.cmd
@@ -1,7 +1,7 @@
 start /wait c:\windows\system32\net stop WinCheck
 start /wait c:\windows\system32\net stop ALTINSOFT
 start /wait c:\windows\system32\net stop ALTINSOFT-Firewall
-del /F /Q C:\Windows\Setup\IPBan\ipban.config
+start /wait cmd /c del /F /Q C:\Windows\Setup\IPBan\ipban.config
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe -OutFile C:\Windows\Setup\LicenseChecker\wincheck.exe"
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/ALTINSOFT.IPBan.exe -OutFile C:\Windows\Setup\IPBan\ALTINSOFT.IPBan.exe"
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/ALTINSOFT.IPBanCore.dll -OutFile C:\Windows\Setup\IPBan\ALTINSOFT.IPBanCore.dll"
