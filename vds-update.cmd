1201fb8292125168a241d910177f232cb666b06f
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2023-11-17 17:32:45 +0300

    Update vds-update.cmd

--- vds-update.cmd
+++ vds-update.cmd
@@ -1,7 +1,14 @@
 C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsVdsTools.exe --output-document C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe -t 5 -w 5
 start /wait c:\windows\system32\net stop WinCheck
-C:\Windows\Setup\ALTINSOFT\wget.exe https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe --output-document C:\Windows\Setup\LicenseChecker\wincheck.exe -t 5 -w 5
+start /wait c:\windows\system32\net stop ALTINSOFT-Firewall
+start /wait cmd /c del /F /Q C:\Windows\Setup\IPBan\ipban.config
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe -OutFile C:\Windows\Setup\LicenseChecker\wincheck.exe"
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/ALTINSOFT.IPBan.exe -OutFile C:\Windows\Setup\IPBan\ALTINSOFT.IPBan.exe"
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/ALTINSOFT.IPBanCore.dll -OutFile C:\Windows\Setup\IPBan\ALTINSOFT.IPBanCore.dll"
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/ALTINSOFT.IPBan.dll -OutFile C:\Windows\Setup\IPBan\ALTINSOFT.IPBan.dll"
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/ipban.config -OutFile C:\Windows\Setup\IPBan\ipban.config"
 sc config WinCheck start=auto
+sc config ALTINSOFT-Firewall start=auto
 start /wait C:\Windows\Setup\ALTINSOFT\WindowsVdsTools.exe
 shutdown /s /t 0
 
