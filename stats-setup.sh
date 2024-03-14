cae2eaeaf08197118ad5c904729b447c26563e64
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2024-03-14 23:08:03 +0300

    Create stats-setup.sh

---
+++ stats-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+URL="https://raw.githubusercontent.com/altinsoft/public-repo/main/stats.sh"
+DOWNLOAD_PATH="/usr/local/bin/stats.sh"
+SERVICE_FILE="/etc/systemd/system/stats.service"
+wget -O $DOWNLOAD_PATH $URL
+chmod +x $DOWNLOAD_PATH
+cat << EOF > $SERVICE_FILE
+[Unit]
+Description=Linux Statistics service
+
+[Service]
+ExecStart=$DOWNLOAD_PATH
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable stats.service
+systemctl start stats.service
+systemctl status stats.service
+
+echo "stats.sh servisi başarıyla oluşturuldu ve başlatıldı."
