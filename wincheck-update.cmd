5681f78daa6589c2d9d235f6e4f179742fde8238
AltinSoft Information Technologies <31034706+altinsoft@users.noreply.github.com>
2025-05-21 12:04:24 +0300

    Update wincheck-update.cmd

--- wincheck-update.cmd
+++ wincheck-update.cmd
@@ -2,6 +2,7 @@ start /wait c:\windows\system32\net stop WinCheck
 start /wait c:\windows\system32\sc delete WinCheck
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/wincheck.exe -OutFile C:\Windows\Setup\LicenseChecker\wincheck.exe"
 start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/System.CodeDom.dll -OutFile C:\Windows\Setup\LicenseChecker\System.CodeDom.dll"
+start /wait powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/altinsoft/public-repo/main/WindowsLicenseChecker.exe.config -OutFile C:\Windows\Setup\LicenseChecker\WindowsLicenseChecker.exe.config"
 start /wait c:\windows\system32\sc CREATE "WinCheck" binpath= "C:\Windows\Setup\LicenseChecker\wincheck.exe
 sc config WinCheck start=auto
 start /wait c:\windows\system32\net start WinCheck
